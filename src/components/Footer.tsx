d5edad632599b0deb961873e4192e576121b2cec
Felipe Santibañez-Leal <felipe@FASL.Work>
2026-04-10 15:09:03 -0400

    fix: separate home and inner chrome variants

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import {
   contactInfo,
@@ -8,37 +8,47 @@ import {
 import { SocialIcon } from "./SocialIcon";
 
 export function Footer() {
-  return (
-    <footer className="site-footer site-footer--seamless">
-      <div className="site-footer__mountain" aria-hidden="true">
-        <svg viewBox="0 0 1920 720" preserveAspectRatio="none">
-          <defs>
-            <linearGradient
-              id="footerMountainGradient"
-              x1="2741.4871"
-              y1="2717.3525"
-              x2="-1771.3439"
-              y2="1921.6184"
-              gradientTransform="matrix(0.9848 -0.1736 0.1736 0.9848 11.3419 105.8396)"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset="0" stopColor="#040c5e" />
-              <stop offset="0.2856" stopColor="#040c5e" />
-              <stop offset="1" stopColor="#040c5e" />
-            </linearGradient>
-          </defs>
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
-          <path
-            fill="url(#footerMountainGradient)"
-            d="M-1381.2,2764.3l320.5,793.3c13.1,32.5,32.9,61.8,58.1,86.2l615.4,594.3c25.2,24.3,55.2,43.1,88.2,55.1
-            l804,292.6c32.9,12,68,16.9,102.9,14.5l853.5-59.7c34.9-2.4,69-12.2,99.9-28.7l755.4-401.7c30.9-16.4,58.1-39.2,79.6-66.8
-            l526.7-674.2c21.6-27.6,37.1-59.4,45.6-93.4l207-830.1c8.5-34,9.7-69.4,3.6-103.9l-148.6-842.6c-6.1-34.5-19.4-67.3-38.9-96.4
-            l-478.4-709.3c-19.6-29-45.1-53.6-74.8-72.2L1713,168c-29.7-18.6-63-30.7-97.7-35.6L768.1,13.3c-34.7-4.9-70-2.4-103.7,7.3
-            L-158,256.4c-33.7,9.7-64.9,26.3-91.8,48.8l-655.4,549.9c-26.8,22.5-48.6,50.4-64,81.9l-375,769c-15.4,31.5-23.9,65.9-25.1,100.9
-            l-29.9,855C-1400.5,2696.9-1394.3,2731.8-1381.2,2764.3z"
-          />
-        </svg>
-      </div>
+  return (
+    <footer
+      className={[
+        "site-footer",
+        isHome ? "site-footer--seamless" : "site-footer--standard",
+      ].join(" ")}
+    >
+      {isHome ? (
+        <div className="site-footer__mountain" aria-hidden="true">
+          <svg viewBox="0 0 1920 720" preserveAspectRatio="none">
+            <defs>
+              <linearGradient
+                id="footerMountainGradient"
+                x1="2741.4871"
+                y1="2717.3525"
+                x2="-1771.3439"
+                y2="1921.6184"
+                gradientTransform="matrix(0.9848 -0.1736 0.1736 0.9848 11.3419 105.8396)"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stopColor="#040c5e" />
+                <stop offset="0.2856" stopColor="#040c5e" />
+                <stop offset="1" stopColor="#040c5e" />
+              </linearGradient>
+            </defs>
+
+            <path
+              fill="url(#footerMountainGradient)"
+              d="M-1381.2,2764.3l320.5,793.3c13.1,32.5,32.9,61.8,58.1,86.2l615.4,594.3c25.2,24.3,55.2,43.1,88.2,55.1
+              l804,292.6c32.9,12,68,16.9,102.9,14.5l853.5-59.7c34.9-2.4,69-12.2,99.9-28.7l755.4-401.7c30.9-16.4,58.1-39.2,79.6-66.8
+              l526.7-674.2c21.6-27.6,37.1-59.4,45.6-93.4l207-830.1c8.5-34,9.7-69.4,3.6-103.9l-148.6-842.6c-6.1-34.5-19.4-67.3-38.9-96.4
+              l-478.4-709.3c-19.6-29-45.1-53.6-74.8-72.2L1713,168c-29.7-18.6-63-30.7-97.7-35.6L768.1,13.3c-34.7-4.9-70-2.4-103.7,7.3
+              L-158,256.4c-33.7,9.7-64.9,26.3-91.8,48.8l-655.4,549.9c-26.8,22.5-48.6,50.4-64,81.9l-375,769c-15.4,31.5-23.9,65.9-25.1,100.9
+              l-29.9,855C-1400.5,2696.9-1394.3,2731.8-1381.2,2764.3z"
+            />
+          </svg>
+        </div>
+      ) : null}
 
       <div className="container site-footer__grid">
         <div className="site-footer__brand" data-reveal>
